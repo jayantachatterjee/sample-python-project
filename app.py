b745af9525ddd25521c00bc9554483b1302bc9ea
jayantachatterjee <rajibjoy@rediffmail.com>
2025-11-07 19:07:00 +0530

    update application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Dockerized Flask! This is written and deployed by Jayanta Chatterjee.'
+    return 'Hello, This response is from ECS and CodePipeline! This is written and deployed by Jayanta Chatterjee.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
